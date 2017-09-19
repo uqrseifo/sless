0cfd4bf81e7af3721fdd447b8141a50723b22579
uqrseifo <r.seifouri@uq.edu.au>
2017-09-19 12:47:14 +1000

    update #3

--- handler.js
+++ handler.js
@@ -1,14 +1,20 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello from Serverless!',
-      input: event,
-    }),
-  };
+var http = require('https');
+module.exports.hrea = (event, context, callback) => {
+//  const response = {
+//    statusCode: 200,
+//    body: JSON.stringify({
+//      message: 'Hello from Serverless Test #3!',
+//      input: event,
+//    }),
+//  };
 
+  http.get('https://hrea-Production.ads.aws.uq.edu.au/RUNME', function (result) {
+      context.done(null);
+    }).on('error', function (err) {
+      context.done("Failed");
+    }); 
   callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
